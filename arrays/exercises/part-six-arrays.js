78dec81c498166f1a294d16b2446ec49848fc5f4
Parvathy <parvathyvenu14@gmail.com>
2024-06-24 16:47:03 -0500

    Class 4 Exercises for & while  loops

--- arrays/exercises/part-six-arrays.js
+++ arrays/exercises/part-six-arrays.js
@@ -1,10 +1,14 @@
 //Arrays can hold different data types, even other arrays! A multi-dimensional array is one with entries that are themselves arrays.
 
+
 //1) Define and initialize the arrays specified in the exercise to hold the name, chemical symbol and mass for different elements.
+
 let element1 = ['hydrogen', 'H', 1.008];
 let element2 = ['helium', 'He', 4.003];
 let element26 = ['iron', 'Fe', 55.85];
+
 //2) Define the array 'table', and use 'push' to add each of the element arrays to it. Print 'table' to see its structure.
+
 let table = [];
 table.push(element1);
 table.push(element2);
@@ -12,27 +16,31 @@ table.push(element26);
 console.log(table);
 
 //3) Use bracket notation to examine the difference between printing 'table' with one index vs. two indices (table[][]).
+
 console.log(table[1], table[1][1]);
+
 //4) Using bracket notation and the table array, print the mass of element1, the name for element 2 and the symbol for element26.
+
 let massOfElement1 = table[0][2];
 let nameOfElement = table[1][0];
 let symbolOfElement = table[2][1];
 console.log("Mass of Element = "+massOfElement1);
 console.log("Name of Element = "+nameOfElement);
 console.log("Symbol of Element = "+symbolOfElement);
+
 //5) 'table' is an example of a 2-dimensional array. The first “level” contains the element arrays, and the second level holds the name/symbol/mass values. Experiment! Create a 3-dimensional array and print out one entry from each level in the array.
 
-let threedimentional = [];
+let threeDimentional = [];
 
-threedimentional[0]=[];
-threedimentional[0].push('element1');
-threedimentional[0].push(table[0]);
-threedimentional[1]=[];
-threedimentional[1].push('element2');
-threedimentional[1].push(table[1]);
-threedimentional[2]=[];
-threedimentional[2].push('element3');
-threedimentional[2].push(table[2]);
+threeDimentional[0]=[];
+threeDimentional[0].push('element1');
+threeDimentional[0].push(table[0]);
+threeDimentional[1]=[];
+threeDimentional[1].push('element2');
+threeDimentional[1].push(table[1]);
+threeDimentional[2]=[];
+threeDimentional[2].push('element3');
+threeDimentional[2].push(table[2]);
 
-console.log(threedimentional);
-console.log(threedimentional[0][0] + ' => ' + threedimentional[0][1][0]);
+console.log(threeDimentional);
+console.log(threeDimentional[0][0] + ' => ' + threeDimentional[0][1][0]);
--- arrays/studio/string-modification.js
+++ arrays/studio/string-modification.js
@@ -3,8 +3,8 @@ let str = "LaunchCode";
 
 //1) Use string methods to remove the first three characters from the string and add them to the end.
 //Hint - define another variable to hold the new string or reassign the new string to str.
-let firstThreeCharacter = str.slice(3);
-let nextCharacter = str.slice(0,3);
+let firstThreeCharacter = str.slice(0,3);
+let nextCharacter = str.slice(3);
 let newString = nextCharacter + firstThreeCharacter;
 
 
@@ -20,7 +20,8 @@ let firstCharacters =str.slice(0, numCharInteger);
 let lastCharacters = str.slice(numCharInteger);
 let newNewString = lastCharacters + firstCharacters;
 
-
+console.log(newNewString);
 
 
 //3) Add validation to your code to deal with user inputs that are longer than the word. In such cases, default to moving 3 characters. Also, the template literal should note the error.
+
