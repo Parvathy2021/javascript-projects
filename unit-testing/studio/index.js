fd379a093baf147b61d7a8afef0ebf44d172d585
Parvathy <parvathyvenu14@gmail.com>
2024-07-17 11:39:59 -0500

    CH14 - studio

--- unit-testing/studio/index.js
+++ unit-testing/studio/index.js
@@ -1,7 +1,28 @@
-
 let launchcode = {
-  
-}
+    organization : "nonprofit",
+    executiveDirector: "Jeff",
+    percentageCoolEmployees: 100,
+    programsOffered: ["Web Development", "Data Analysis", "Liftoff"],
+    launchOutput: function(number){
+      
+      if(number % 2 ===0 && number % 3 ===0 && number % 5 ===0){
+        return 'LaunchCode Rocks!';
 
+      }else if(number % 2 ===0 && number % 5 ===0){
+        return 'Launch Rocks! (CRASH!!!!)';
+      }if(number % 3 ===0 && number % 5 ===0){
+        return 'Code Rocks!';
+      }else if(number % 2 ===0 && number % 3 ===0){
+        return 'LaunchCode!';
+      }else if(number % 2 ===0){
+        return 'Launch!';
+      } else if(number % 3 ===0){
+        return 'Code!';
+      }else if(number % 5 ===0){
+        return 'Rocks!';
+      } else 
+      return V="Rutabagas! That doesn't work.'";
+    } 
+  }
 module.exports = launchcode;
 
